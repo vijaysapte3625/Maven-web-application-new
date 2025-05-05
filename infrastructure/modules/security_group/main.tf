feb2d085554c937c44e9c80335a154083786719e
hanzel_sc <hanselkansam04@gmail.com>
2025-05-05 11:52:36 +0530

    Added Terraform infrastructure modules and Jenkins Pipeline for Nexus Deployment

--- infrastructure/modules/security_group/main.tf
+++ infrastructure/modules/security_group/main.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080 #For the Jenkins
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
